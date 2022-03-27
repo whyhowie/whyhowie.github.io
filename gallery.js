22467b83e95074856ec23db585b666ca24c711a8
howiewang2 <howiewang@cmu.edu>
2022-03-27 14:46:23 -0400

    added more elements!

--- gallery.js
+++ gallery.js
@@ -1,18 +1,25 @@
-$(document).ready(function() {
-  const gallery = document.querySelector('#paginated_gallery');
-  const gallery_scroller = gallery.querySelector('.gallery_scroller');
-  const gallery_item_size = gallery_scroller.querySelector('div').clientWidth;
+$(function() {
+  const galleries = document.querySelectorAll('.gallery');
+  console.log(galleries.length);
 
-  gallery.querySelector('.btn.next').addEventListener('click', scrollToNextPage);
-  gallery.querySelector('.btn.prev').addEventListener('click', scrollToPrevPage);
+
+  for (var i=0; i<2; i++) {
+    let gallery = galleries[i];
+    let gallery_scroller = gallery.querySelector('.gallery_scroller');
+    let gallery_item_size = gallery_scroller.querySelector('div').clientWidth;
+
+    gallery.querySelector('.btn.next').addEventListener('click', scrollToNextPage);
+    gallery.querySelector('.btn.prev').addEventListener('click', scrollToPrevPage);
+
+    function scrollToNextPage() {
+      gallery_scroller.scrollTo(gallery_scroller.scrollLeft + gallery_item_size, 0);
+    }
+    function scrollToPrevPage() {
+      gallery_scroller.scrollTo(gallery_scroller.scrollLeft - gallery_item_size, 0);
+    }
+  }
 
   // For paginated scrolling, simply scroll the gallery one item in the given
   // direction and let css scroll snaping handle the specific alignment.
-  function scrollToNextPage() {
-    gallery_scroller.scrollTo(gallery_scroller.scrollLeft + gallery_item_size, 0);
-  }
-  function scrollToPrevPage() {
-    gallery_scroller.scrollTo(gallery_scroller.scrollLeft - gallery_item_size, 0);
-  }
 
 });
