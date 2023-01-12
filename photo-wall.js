ade08783762f7658b07f1c5a1f396666ac448128
howiewang2 <howiewang@cmu.edu>
2023-01-12 13:51:41 -0500

    Updated first page and gallery

---
+++ photo-wall.js
@@ -0,0 +1,20 @@
+// Make image lightboxes
+// https://stackoverflow.com/questions/10910537/how-to-show-an-image-in-a-popup-on-click
+$('body').append(`<div id="larger-image"></div>
+  <div id="image-overlay"></div>`)
+
+// Fade-in
+$('.photo-wall-columns img').click(function () {
+  var projectTitle = $(this).closest(".project").children("h2")[0].textContent
+  var $img = $(this);
+  $('#larger-image').html($img.clone().add(`<p id="project-image-title">${projectTitle}</p>`)).add($('#image-overlay')).fadeIn();
+});
+
+// Fade-out
+$('#larger-image').add($('#image-overlay')).click(function () {
+  $('#larger-image').add($('#image-overlay')).fadeOut(
+    function () {
+      $('#larger-image').empty();
+    }
+  )
+})
